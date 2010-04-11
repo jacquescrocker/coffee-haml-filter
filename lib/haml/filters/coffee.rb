7af124fea9772a0c2c723d1c919c37d8a1565e4c
Gerad Suyderhoud <gerads@gmail.com>
2010-04-11 08:53:55 -0700

    remove old gem dependency

--- lib/haml/filters/coffee.rb
+++ lib/haml/filters/coffee.rb
@@ -1,13 +1,14 @@
 module Haml::Filters::Coffee
   include Haml::Filters::Base
 
-  lazy_require 'coffee-script'
-
   def render_with_options(text, options)
     <<END
 <script type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}>
   //<![CDATA[
-    #{CoffeeScript.compile(text)}
+    IO.popen('coffee -s', 'rw') do |c|
+      c.puts text
+      c.readlines.join("\n")
+    end
   //]]>
 </script>
 END
