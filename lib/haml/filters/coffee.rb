dc078a5b1265c020f66ed798b3f5df3df8cd20a9
Gerad Suyderhoud <gerads@gmail.com>
2010-04-11 14:05:44 -0700

    bug fixes

--- lib/haml/filters/coffee.rb
+++ lib/haml/filters/coffee.rb
@@ -2,13 +2,15 @@ module Haml::Filters::Coffee
   include Haml::Filters::Base
 
   def render_with_options(text, options)
-    <<END
-<script type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}>
-  //<![CDATA[
-    IO.popen('coffee -s', 'rw') do |c|
-      c.puts text
-      c.readlines.join("\n")
+    js = IO.popen('coffee -sc', 'r+') do |c|
+      c << text
+      c.close_write
+      c.read
     end
+    <<END
+<script type="#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}">
+  //<![CDATA[
+    #{js}
   //]]>
 </script>
 END
