6c0edbdfb28feb0e231cdcbdef637934a5ad3408
Alan Harper <alan@aussiegeek.net>
2010-05-22 13:12:05 +1000

    Allow changing the path to the coffee binary

--- lib/haml/filters/coffee.rb
+++ lib/haml/filters/coffee.rb
@@ -1,10 +1,26 @@
 module Haml::Filters::Coffee
+  attr_accessor :coffee_path
   include Haml::Filters::Base
 
   lazy_require 'open3'
 
+  def coffee_path
+    @coffee_path || "coffee"
+  end
+
+  def popen_args
+    bin, args = coffee_path.split(' ')
+    if args
+      args << " -sc"
+    else
+      args = "-sc"
+    end
+
+    [bin, args]
+  end
   def render_with_options(text, options)
-    js, error = Open3.popen3('coffee', '-sc') do |i,o,e|
+    bin, path = popen_args
+    js, error = Open3.popen3(bin, path) do |i,o,e|
       i << text
       i.close
       [o.read, e.read]
--- spec/coffee-haml-filter_spec.rb
+++ spec/coffee-haml-filter_spec.rb
@@ -12,4 +12,29 @@ describe "CoffeeHamlFilter" do
     template = read_fixture('invalid.haml')
     lambda { Haml::Engine.new(template) }.should raise_error(SyntaxError)
   end
+
+  describe "setup" do
+    describe "default path" do
+      it "should have the correct path" do
+        Haml::Filters::Coffee.coffee_path.should == "coffee"
+      end
+
+      it "should generate popen args" do
+        Haml::Filters::Coffee.popen_args.should == ["coffee", "-sc"]
+      end
+    end
+
+    describe "should change the path if requested" do
+      before do
+        Haml::Filters::Coffee.coffee_path = "/usr/local/bin/node /usr/local/bin/coffee"
+      end
+      it "should update coffee path" do
+        Haml::Filters::Coffee.coffee_path.should == "/usr/local/bin/node /usr/local/bin/coffee"
+      end
+
+      it "should generate popen args" do
+        Haml::Filters::Coffee.popen_args.should == ["/usr/local/bin/node", "/usr/local/bin/coffee -sc"]
+      end
+    end
+  end
 end
